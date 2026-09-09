3e784d3c916a8b39bbe01c913e6b928c0d6f6c51
Mnw1995 <mnw@1995.com>
2026-09-09 23:35:22 +0200

    Add shell script which run the Python script to generate test skeletons on all headers in D2Game

---
+++ source/D2Game/generate_test_skeletons.sh
@@ -0,0 +1,156 @@
+# AI
+
+python ..\\generate_test_skeletons.py D2Game 0x6FC30000 AiBaalTests .\\include\\AI\\AiBaal.h .\\tests\\AI\\AiBaalTests.cpp
+python ..\\generate_test_skeletons.py D2Game 0x6FC30000 AiGeneralTests .\\include\\AI\\AiGeneral.h .\\tests\\AI\\AiGeneralTests.cpp
+python ..\\generate_test_skeletons.py D2Game 0x6FC30000 AiTacticsTests .\\include\\AI\\AiTactics.h .\\tests\\AI\\AiTacticsTests.cpp
+python ..\\generate_test_skeletons.py D2Game 0x6FC30000 AiThinkTests .\\include\\AI\\AiThink.h .\\tests\\AI\\AiThinkTests.cpp
+python ..\\generate_test_skeletons.py D2Game 0x6FC30000 AiUtilTests .\\include\\AI\\AiUtil.h .\\tests\\AI\\AiUtilTests.cpp
+
+
+# DEBUG
+
+python ..\\generate_test_skeletons.py D2Game 0x6FC30000 DebugTests .\\include\\DEBUG\\Debug.h .\\tests\\DEBUG\\DebugTests.cpp
+
+
+# GAME
+
+python ..\\generate_test_skeletons.py D2Game 0x6FC30000 ArenaTests .\\include\\GAME\\Arena.h .\\tests\\GAME\\ArenaTests.cpp
+python ..\\generate_test_skeletons.py D2Game 0x6FC30000 CCmdTests .\\include\\GAME\\CCmd.h .\\tests\\GAME\\CCmdTests.cpp
+python ..\\generate_test_skeletons.py D2Game 0x6FC30000 ClientsTests .\\include\\GAME\\Clients.h .\\tests\\GAME\\ClientsTests.cpp
+python ..\\generate_test_skeletons.py D2Game 0x6FC30000 EventTests .\\include\\GAME\\Event.h .\\tests\\GAME\\EventTests.cpp
+python ..\\generate_test_skeletons.py D2Game 0x6FC30000 GameTests .\\include\\GAME\\Game.h .\\tests\\GAME\\GameTests.cpp
+python ..\\generate_test_skeletons.py D2Game 0x6FC30000 LevelTests .\\include\\GAME\\Level.h .\\tests\\GAME\\LevelTests.cpp
+python ..\\generate_test_skeletons.py D2Game 0x6FC30000 SCmdTests .\\include\\GAME\\SCmd.h .\\tests\\GAME\\SCmdTests.cpp
+python ..\\generate_test_skeletons.py D2Game 0x6FC30000 TargetsTests .\\include\\GAME\\Targets.h .\\tests\\GAME\\TargetsTests.cpp
+python ..\\generate_test_skeletons.py D2Game 0x6FC30000 TaskTests .\\include\\GAME\\Task.h .\\tests\\GAME\\TaskTests.cpp
+
+
+# INVENTORY
+
+python ..\\generate_test_skeletons.py D2Game 0x6FC30000 InvModeTests .\\include\\INVENTORY\\InvMode.h .\\tests\\INVENTORY\\InvModeTests.cpp
+
+
+# ITEMS
+
+python ..\\generate_test_skeletons.py D2Game 0x6FC30000 ItemModeTests .\\include\\ITEMS\\ItemMode.h .\\tests\\ITEMS\\ItemModeTests.cpp
+python ..\\generate_test_skeletons.py D2Game 0x6FC30000 ItemsTests .\\include\\ITEMS\\Items.h .\\tests\\ITEMS\\ItemsTests.cpp
+python ..\\generate_test_skeletons.py D2Game 0x6FC30000 ItemsMagicTests .\\include\\ITEMS\\ItemsMagic.h .\\tests\\ITEMS\\ItemsMagicTests.cpp
+
+
+# MISSILES
+
+python ..\\generate_test_skeletons.py D2Game 0x6FC30000 MissilesTests .\\include\\MISSILES\\Missiles.h .\\tests\\MISSILES\\MissilesTests.cpp
+python ..\\generate_test_skeletons.py D2Game 0x6FC30000 MissModeTests .\\include\\MISSILES\\MissMode.h .\\tests\\MISSILES\\MissModeTests.cpp
+
+
+# MONSTER
+
+python ..\\generate_test_skeletons.py D2Game 0x6FC30000 MonsterTests .\\include\\MONSTER\\Monster.h .\\tests\\MONSTER\\MonsterTests.cpp
+python ..\\generate_test_skeletons.py D2Game 0x6FC30000 MonsterAITests .\\include\\MONSTER\\MonsterAI.h .\\tests\\MONSTER\\MonsterAITests.cpp
+python ..\\generate_test_skeletons.py D2Game 0x6FC30000 MonsterChooseTests .\\include\\MONSTER\\MonsterChoose.h .\\tests\\MONSTER\\MonsterChooseTests.cpp
+python ..\\generate_test_skeletons.py D2Game 0x6FC30000 MonsterModeTests .\\include\\MONSTER\\MonsterMode.h .\\tests\\MONSTER\\MonsterModeTests.cpp
+python ..\\generate_test_skeletons.py D2Game 0x6FC30000 MonsterMsgTests .\\include\\MONSTER\\MonsterMsg.h .\\tests\\MONSTER\\MonsterMsgTests.cpp
+python ..\\generate_test_skeletons.py D2Game 0x6FC30000 MonsterRegionTests .\\include\\MONSTER\\MonsterRegion.h .\\tests\\MONSTER\\MonsterRegionTests.cpp
+python ..\\generate_test_skeletons.py D2Game 0x6FC30000 MonsterSpawnTests .\\include\\MONSTER\\MonsterSpawn.h .\\tests\\MONSTER\\MonsterSpawnTests.cpp
+python ..\\generate_test_skeletons.py D2Game 0x6FC30000 MonsterUniqueTests .\\include\\MONSTER\\MonsterUnique.h .\\tests\\MONSTER\\MonsterUniqueTests.cpp
+
+
+# OBJECTS
+
+python ..\\generate_test_skeletons.py D2Game 0x6FC30000 ObjectsTests .\\include\\OBJECTS\\Objects.h .\\tests\\OBJECTS\\ObjectsTests.cpp
+python ..\\generate_test_skeletons.py D2Game 0x6FC30000 ObjEvalTests .\\include\\OBJECTS\\ObjEval.h .\\tests\\OBJECTS\\ObjEvalTests.cpp
+python ..\\generate_test_skeletons.py D2Game 0x6FC30000 ObjModeTests .\\include\\OBJECTS\\ObjMode.h .\\tests\\OBJECTS\\ObjModeTests.cpp
+python ..\\generate_test_skeletons.py D2Game 0x6FC30000 ObjRgnTests .\\include\\OBJECTS\\ObjRgn.h .\\tests\\OBJECTS\\ObjRgnTests.cpp
+
+
+# PLAYER
+
+python ..\\generate_test_skeletons.py D2Game 0x6FC30000 PartyScreenTests .\\include\\PLAYER\\PartyScreen.h .\\tests\\PLAYER\\PartyScreenTests.cpp
+python ..\\generate_test_skeletons.py D2Game 0x6FC30000 PlayerTests .\\include\\PLAYER\\Player.h .\\tests\\PLAYER\\PlayerTests.cpp
+python ..\\generate_test_skeletons.py D2Game 0x6FC30000 PlayerListTests .\\include\\PLAYER\\PlayerList.h .\\tests\\PLAYER\\PlayerListTests.cpp
+python ..\\generate_test_skeletons.py D2Game 0x6FC30000 PlayerPetsTests .\\include\\PLAYER\\PlayerPets.h .\\tests\\PLAYER\\PlayerPetsTests.cpp
+python ..\\generate_test_skeletons.py D2Game 0x6FC30000 PlayerStatsTests .\\include\\PLAYER\\PlayerStats.h .\\tests\\PLAYER\\PlayerStatsTests.cpp
+python ..\\generate_test_skeletons.py D2Game 0x6FC30000 PlrIntroTests .\\include\\PLAYER\\PlrIntro.h .\\tests\\PLAYER\\PlrIntroTests.cpp
+python ..\\generate_test_skeletons.py D2Game 0x6FC30000 PlrModesTests .\\include\\PLAYER\\PlrModes.h .\\tests\\PLAYER\\PlrModesTests.cpp
+python ..\\generate_test_skeletons.py D2Game 0x6FC30000 PlrMsgTests .\\include\\PLAYER\\PlrMsg.h .\\tests\\PLAYER\\PlrMsgTests.cpp
+python ..\\generate_test_skeletons.py D2Game 0x6FC30000 PlrSaveTests .\\include\\PLAYER\\PlrSave.h .\\tests\\PLAYER\\PlrSaveTests.cpp
+python ..\\generate_test_skeletons.py D2Game 0x6FC30000 PlrSave2Tests .\\include\\PLAYER\\PlrSave2.h .\\tests\\PLAYER\\PlrSave2Tests.cpp
+python ..\\generate_test_skeletons.py D2Game 0x6FC30000 PlrTradeTests .\\include\\PLAYER\\PlrTrade.h .\\tests\\PLAYER\\PlrTradeTests.cpp
+
+
+# QUESTS
+
+python ..\\generate_test_skeletons.py D2Game 0x6FC30000 A1IntroTests .\\include\\QUESTS\\ACT1\\A1Intro.h .\\tests\\QUESTS\\ACT1\\A1IntroTests.cpp
+python ..\\generate_test_skeletons.py D2Game 0x6FC30000 A1Q0Tests .\\include\\QUESTS\\ACT1\\A1Q0.h .\\tests\\QUESTS\\ACT1\\A1Q0Tests.cpp
+python ..\\generate_test_skeletons.py D2Game 0x6FC30000 A1Q1Tests .\\include\\QUESTS\\ACT1\\A1Q1.h .\\tests\\QUESTS\\ACT1\\A1Q1Tests.cpp
+python ..\\generate_test_skeletons.py D2Game 0x6FC30000 A1Q2Tests .\\include\\QUESTS\\ACT1\\A1Q2.h .\\tests\\QUESTS\\ACT1\\A1Q2Tests.cpp
+python ..\\generate_test_skeletons.py D2Game 0x6FC30000 A1Q3Tests .\\include\\QUESTS\\ACT1\\A1Q3.h .\\tests\\QUESTS\\ACT1\\A1Q3Tests.cpp
+python ..\\generate_test_skeletons.py D2Game 0x6FC30000 A1Q4Tests .\\include\\QUESTS\\ACT1\\A1Q4.h .\\tests\\QUESTS\\ACT1\\A1Q4Tests.cpp
+python ..\\generate_test_skeletons.py D2Game 0x6FC30000 A1Q5Tests .\\include\\QUESTS\\ACT1\\A1Q5.h .\\tests\\QUESTS\\ACT1\\A1Q5Tests.cpp
+python ..\\generate_test_skeletons.py D2Game 0x6FC30000 A1Q6Tests .\\include\\QUESTS\\ACT1\\A1Q6.h .\\tests\\QUESTS\\ACT1\\A1Q6Tests.cpp
+python ..\\generate_test_skeletons.py D2Game 0x6FC30000 A1Q7Tests .\\include\\QUESTS\\ACT1\\A1Q7.h .\\tests\\QUESTS\\ACT1\\A1Q7Tests.cpp
+
+python ..\\generate_test_skeletons.py D2Game 0x6FC30000 A2IntroTests .\\include\\QUESTS\\ACT2\\A2Intro.h .\\tests\\QUESTS\\ACT2\\A2IntroTests.cpp
+python ..\\generate_test_skeletons.py D2Game 0x6FC30000 A2Q0Tests .\\include\\QUESTS\\ACT2\\A2Q0.h .\\tests\\QUESTS\\ACT2\\A2Q0Tests.cpp
+python ..\\generate_test_skeletons.py D2Game 0x6FC30000 A2Q1Tests .\\include\\QUESTS\\ACT2\\A2Q1.h .\\tests\\QUESTS\\ACT2\\A2Q1Tests.cpp
+python ..\\generate_test_skeletons.py D2Game 0x6FC30000 A2Q2Tests .\\include\\QUESTS\\ACT2\\A2Q2.h .\\tests\\QUESTS\\ACT2\\A2Q2Tests.cpp
+python ..\\generate_test_skeletons.py D2Game 0x6FC30000 A2Q3Tests .\\include\\QUESTS\\ACT2\\A2Q3.h .\\tests\\QUESTS\\ACT2\\A2Q3Tests.cpp
+python ..\\generate_test_skeletons.py D2Game 0x6FC30000 A2Q4Tests .\\include\\QUESTS\\ACT2\\A2Q4.h .\\tests\\QUESTS\\ACT2\\A2Q4Tests.cpp
+python ..\\generate_test_skeletons.py D2Game 0x6FC30000 A2Q5Tests .\\include\\QUESTS\\ACT2\\A2Q5.h .\\tests\\QUESTS\\ACT2\\A2Q5Tests.cpp
+python ..\\generate_test_skeletons.py D2Game 0x6FC30000 A2Q6Tests .\\include\\QUESTS\\ACT2\\A2Q6.h .\\tests\\QUESTS\\ACT2\\A2Q6Tests.cpp
+python ..\\generate_test_skeletons.py D2Game 0x6FC30000 A2Q7Tests .\\include\\QUESTS\\ACT2\\A2Q7.h .\\tests\\QUESTS\\ACT2\\A2Q7Tests.cpp
+python ..\\generate_test_skeletons.py D2Game 0x6FC30000 A2Q8Tests .\\include\\QUESTS\\ACT2\\A2Q8.h .\\tests\\QUESTS\\ACT2\\A2Q8Tests.cpp
+
+python ..\\generate_test_skeletons.py D2Game 0x6FC30000 A3IntroTests .\\include\\QUESTS\\ACT3\\A3Intro.h .\\tests\\QUESTS\\ACT3\\A3IntroTests.cpp
+python ..\\generate_test_skeletons.py D2Game 0x6FC30000 A3Q0Tests .\\include\\QUESTS\\ACT3\\A3Q0.h .\\tests\\QUESTS\\ACT3\\A3Q0Tests.cpp
+python ..\\generate_test_skeletons.py D2Game 0x6FC30000 A3Q1Tests .\\include\\QUESTS\\ACT3\\A3Q1.h .\\tests\\QUESTS\\ACT3\\A3Q1Tests.cpp
+python ..\\generate_test_skeletons.py D2Game 0x6FC30000 A3Q2Tests .\\include\\QUESTS\\ACT3\\A3Q2.h .\\tests\\QUESTS\\ACT3\\A3Q2Tests.cpp
+python ..\\generate_test_skeletons.py D2Game 0x6FC30000 A3Q3Tests .\\include\\QUESTS\\ACT3\\A3Q3.h .\\tests\\QUESTS\\ACT3\\A3Q3Tests.cpp
+python ..\\generate_test_skeletons.py D2Game 0x6FC30000 A3Q4Tests .\\include\\QUESTS\\ACT3\\A3Q4.h .\\tests\\QUESTS\\ACT3\\A3Q4Tests.cpp
+python ..\\generate_test_skeletons.py D2Game 0x6FC30000 A3Q5Tests .\\include\\QUESTS\\ACT3\\A3Q5.h .\\tests\\QUESTS\\ACT3\\A3Q5Tests.cpp
+python ..\\generate_test_skeletons.py D2Game 0x6FC30000 A3Q6Tests .\\include\\QUESTS\\ACT3\\A3Q6.h .\\tests\\QUESTS\\ACT3\\A3Q6Tests.cpp
+python ..\\generate_test_skeletons.py D2Game 0x6FC30000 A3Q7Tests .\\include\\QUESTS\\ACT3\\A3Q7.h .\\tests\\QUESTS\\ACT3\\A3Q7Tests.cpp
+
+python ..\\generate_test_skeletons.py D2Game 0x6FC30000 A4Q0Tests .\\include\\QUESTS\\ACT4\\A4Q0.h .\\tests\\QUESTS\\ACT4\\A4Q0Tests.cpp
+python ..\\generate_test_skeletons.py D2Game 0x6FC30000 A4Q1Tests .\\include\\QUESTS\\ACT4\\A4Q1.h .\\tests\\QUESTS\\ACT4\\A4Q1Tests.cpp
+python ..\\generate_test_skeletons.py D2Game 0x6FC30000 A4Q2Tests .\\include\\QUESTS\\ACT4\\A4Q2.h .\\tests\\QUESTS\\ACT4\\A4Q2Tests.cpp
+python ..\\generate_test_skeletons.py D2Game 0x6FC30000 A4Q3Tests .\\include\\QUESTS\\ACT4\\A4Q3.h .\\tests\\QUESTS\\ACT4\\A4Q3Tests.cpp
+python ..\\generate_test_skeletons.py D2Game 0x6FC30000 A4Q4Tests .\\include\\QUESTS\\ACT4\\A4Q4.h .\\tests\\QUESTS\\ACT4\\A4Q4Tests.cpp
+
+python ..\\generate_test_skeletons.py D2Game 0x6FC30000 A5IntroTests .\\include\\QUESTS\\ACT5\\A5Intro.h .\\tests\\QUESTS\\ACT5\\A5IntroTests.cpp
+python ..\\generate_test_skeletons.py D2Game 0x6FC30000 A5Q1Tests .\\include\\QUESTS\\ACT5\\A5Q1.h .\\tests\\QUESTS\\ACT5\\A5Q1Tests.cpp
+python ..\\generate_test_skeletons.py D2Game 0x6FC30000 A5Q2Tests .\\include\\QUESTS\\ACT5\\A5Q2.h .\\tests\\QUESTS\\ACT5\\A5Q2Tests.cpp
+python ..\\generate_test_skeletons.py D2Game 0x6FC30000 A5Q3Tests .\\include\\QUESTS\\ACT5\\A5Q3.h .\\tests\\QUESTS\\ACT5\\A5Q3Tests.cpp
+python ..\\generate_test_skeletons.py D2Game 0x6FC30000 A5Q4Tests .\\include\\QUESTS\\ACT5\\A5Q4.h .\\tests\\QUESTS\\ACT5\\A5Q4Tests.cpp
+python ..\\generate_test_skeletons.py D2Game 0x6FC30000 A5Q5Tests .\\include\\QUESTS\\ACT5\\A5Q5.h .\\tests\\QUESTS\\ACT5\\A5Q5Tests.cpp
+python ..\\generate_test_skeletons.py D2Game 0x6FC30000 A5Q6Tests .\\include\\QUESTS\\ACT5\\A5Q6.h .\\tests\\QUESTS\\ACT5\\A5Q6Tests.cpp
+
+python ..\\generate_test_skeletons.py D2Game 0x6FC30000 QuestsTests .\\include\\QUESTS\\Quests.h .\\tests\\QUESTS\\QuestsTests.cpp
+python ..\\generate_test_skeletons.py D2Game 0x6FC30000 QuestsFXTests .\\include\\QUESTS\\QuestsFX.h .\\tests\\QUESTS\\QuestsFXTests.cpp
+
+
+# SKILLS
+
+python ..\\generate_test_skeletons.py D2Game 0x6FC30000 SkillAmaTests .\\include\\SKILLS\\SkillAma.h .\\tests\\SKILLS\\SkillAmaTests.cpp
+python ..\\generate_test_skeletons.py D2Game 0x6FC30000 SkillAssTests .\\include\\SKILLS\\SkillAss.h .\\tests\\SKILLS\\SkillAssTests.cpp
+python ..\\generate_test_skeletons.py D2Game 0x6FC30000 SkillBarTests .\\include\\SKILLS\\SkillBar.h .\\tests\\SKILLS\\SkillBarTests.cpp
+python ..\\generate_test_skeletons.py D2Game 0x6FC30000 SkillDruidTests .\\include\\SKILLS\\SkillDruid.h .\\tests\\SKILLS\\SkillDruidTests.cpp
+python ..\\generate_test_skeletons.py D2Game 0x6FC30000 SkillItemTests .\\include\\SKILLS\\SkillItem.h .\\tests\\SKILLS\\SkillItemTests.cpp
+python ..\\generate_test_skeletons.py D2Game 0x6FC30000 SkillMonstTests .\\include\\SKILLS\\SkillMonst.h .\\tests\\SKILLS\\SkillMonstTests.cpp
+python ..\\generate_test_skeletons.py D2Game 0x6FC30000 SkillNecTests .\\include\\SKILLS\\SkillNec.h .\\tests\\SKILLS\\SkillNecTests.cpp
+python ..\\generate_test_skeletons.py D2Game 0x6FC30000 SkillPalTests .\\include\\SKILLS\\SkillPal.h .\\tests\\SKILLS\\SkillPalTests.cpp
+python ..\\generate_test_skeletons.py D2Game 0x6FC30000 SkillsTests .\\include\\SKILLS\\Skills.h .\\tests\\SKILLS\\SkillsTests.cpp
+python ..\\generate_test_skeletons.py D2Game 0x6FC30000 SkillSorTests .\\include\\SKILLS\\SkillSor.h .\\tests\\SKILLS\\SkillSorTests.cpp
+
+
+# UNIT
+
+python ..\\generate_test_skeletons.py D2Game 0x6FC30000 FriendlyTests .\\include\\UNIT\\Friendly.h .\\tests\\UNIT\\FriendlyTests.cpp
+python ..\\generate_test_skeletons.py D2Game 0x6FC30000 PartyTests .\\include\\UNIT\\Party.h .\\tests\\UNIT\\PartyTests.cpp
+python ..\\generate_test_skeletons.py D2Game 0x6FC30000 SUnitTests .\\include\\UNIT\\SUnit.h .\\tests\\UNIT\\SUnitTests.cpp
+python ..\\generate_test_skeletons.py D2Game 0x6FC30000 SUnitDmgTests .\\include\\UNIT\\SUnitDmg.h .\\tests\\UNIT\\SUnitDmgTests.cpp
+python ..\\generate_test_skeletons.py D2Game 0x6FC30000 SUnitEventTests .\\include\\UNIT\\SUnitEvent.h .\\tests\\UNIT\\SUnitEventTests.cpp
+python ..\\generate_test_skeletons.py D2Game 0x6FC30000 SUnitInactiveTests .\\include\\UNIT\\SUnitInactive.h .\\tests\\UNIT\\SUnitInactiveTests.cpp
+python ..\\generate_test_skeletons.py D2Game 0x6FC30000 SUnitMsgTests .\\include\\UNIT\\SUnitMsg.h .\\tests\\UNIT\\SUnitMsgTests.cpp
+python ..\\generate_test_skeletons.py D2Game 0x6FC30000 SUnitNpcTests .\\include\\UNIT\\SUnitNpc.h .\\tests\\UNIT\\SUnitNpcTests.cpp
+python ..\\generate_test_skeletons.py D2Game 0x6FC30000 SUnitProxyTests .\\include\\UNIT\\SUnitProxy.h .\\tests\\UNIT\\SUnitProxyTests.cpp
