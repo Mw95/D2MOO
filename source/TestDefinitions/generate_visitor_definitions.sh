361d07e89627a14c94d4ec6905ee3335b50493c5
Mnw1995 <mnw@1995.com>
2026-09-09 23:34:33 +0200

    Add shell script which will run the Python script to generate VisitorFramework definitions on all headers in D2Common and D2Game

---
+++ source/TestDefinitions/generate_visitor_definitions.sh
@@ -0,0 +1,181 @@
+# D2Common
+
+# DataTbls
+
+python ..\\generate_visitor_definitions.py ..\\D2Common\\include\\DataTbls\\AnimTbls.h .\\include\\D2Common\\DataTbls\\AnimTblsVisitorDefinitions.h
+python ..\\generate_visitor_definitions.py ..\\D2Common\\include\\DataTbls\\ArenaTbls.h .\\include\\D2Common\\DataTbls\\ArenaTblsVisitorDefinitions.h
+python ..\\generate_visitor_definitions.py ..\\D2Common\\include\\DataTbls\\BeltsTbls.h .\\include\\D2Common\\DataTbls\\BeltsTblsVisitorDefinitions.h
+python ..\\generate_visitor_definitions.py ..\\D2Common\\include\\DataTbls\\FieldTbls.h .\\include\\D2Common\\DataTbls\\FieldTblsVisitorDefinitions.h
+python ..\\generate_visitor_definitions.py ..\\D2Common\\include\\DataTbls\\HoradricCube.h .\\include\\D2Common\\DataTbls\\HoradricCubeVisitorDefinitions.h
+python ..\\generate_visitor_definitions.py ..\\D2Common\\include\\DataTbls\\InvTbls.h .\\include\\D2Common\\DataTbls\\InvTblsVisitorDefinitions.h
+python ..\\generate_visitor_definitions.py ..\\D2Common\\include\\DataTbls\\ItemsTbls.h .\\include\\D2Common\\DataTbls\\ItemsTblsVisitorDefinitions.h
+python ..\\generate_visitor_definitions.py ..\\D2Common\\include\\DataTbls\\LevelsTbls.h .\\include\\D2Common\\DataTbls\\LevelsTblsVisitorDefinitions.h
+python ..\\generate_visitor_definitions.py ..\\D2Common\\include\\DataTbls\\MissilesTbls.h .\\include\\D2Common\\DataTbls\\MissilesTblsVisitorDefinitions.h
+python ..\\generate_visitor_definitions.py ..\\D2Common\\include\\DataTbls\\MonsterTbls.h .\\include\\D2Common\\DataTbls\\MonsterTblsVisitorDefinitions.h
+python ..\\generate_visitor_definitions.py ..\\D2Common\\include\\DataTbls\\ObjectsTbls.h .\\include\\D2Common\\DataTbls\\ObjectsTblsVisitorDefinitions.h
+python ..\\generate_visitor_definitions.py ..\\D2Common\\include\\DataTbls\\OverlayTbls.h .\\include\\D2Common\\DataTbls\\OverlayTblsVisitorDefinitions.h
+python ..\\generate_visitor_definitions.py ..\\D2Common\\include\\DataTbls\\SequenceTbls.h .\\include\\D2Common\\DataTbls\\SequenceTblsVisitorDefinitions.h
+python ..\\generate_visitor_definitions.py ..\\D2Common\\include\\DataTbls\\SkillsTbls.h .\\include\\D2Common\\DataTbls\\SkillsTblsVisitorDefinitions.h
+python ..\\generate_visitor_definitions.py ..\\D2Common\\include\\DataTbls\\TokenTbls.h .\\include\\D2Common\\DataTbls\\TokenTblsVisitorDefinitions.h
+
+# Drlg
+
+python ..\\generate_visitor_definitions.py ..\\D2Common\\include\\Drlg\\D2DrlgDrlg.h .\\include\\D2Common\\Drlg\\D2DrlgDrlgVisitorDefinitions.h
+python ..\\generate_visitor_definitions.py ..\\D2Common\\include\\Drlg\\D2DrlgDrlgGrid.h .\\include\\D2Common\\Drlg\\D2DrlgDrlgGridVisitorDefinitions.h
+python ..\\generate_visitor_definitions.py ..\\D2Common\\include\\Drlg\\D2DrlgDrlgLogic.h .\\include\\D2Common\\Drlg\\D2DrlgDrlgLogicVisitorDefinitions.h
+python ..\\generate_visitor_definitions.py ..\\D2Common\\include\\Drlg\\D2DrlgDrlgVer.h .\\include\\D2Common\\Drlg\\D2DrlgDrlgVerVisitorDefinitions.h
+python ..\\generate_visitor_definitions.py ..\\D2Common\\include\\Drlg\\D2DrlgMaze.h .\\include\\D2Common\\Drlg\\D2DrlgMazeVisitorDefinitions.h
+python ..\\generate_visitor_definitions.py ..\\D2Common\\include\\Drlg\\D2DrlgOutdoors.h .\\include\\D2Common\\Drlg\\D2DrlgOutdoorsVisitorDefinitions.h
+python ..\\generate_visitor_definitions.py ..\\D2Common\\include\\Drlg\\D2DrlgOutPlace.h .\\include\\D2Common\\Drlg\\D2DrlgOutPlaceVisitorDefinitions.h
+python ..\\generate_visitor_definitions.py ..\\D2Common\\include\\Drlg\\D2DrlgOutRoom.h .\\include\\D2Common\\Drlg\\D2DrlgOutRoomVisitorDefinitions.h
+python ..\\generate_visitor_definitions.py ..\\D2Common\\include\\Drlg\\D2DrlgPreset.h .\\include\\D2Common\\Drlg\\D2DrlgPresetVisitorDefinitions.h
+python ..\\generate_visitor_definitions.py ..\\D2Common\\include\\Drlg\\D2DrlgTileSub.h .\\include\\D2Common\\Drlg\\D2DrlgTileSubVisitorDefinitions.h
+
+# PaVisitorDefinitions.h
+
+python ..\\generate_visitor_definitions.py ..\\D2Common\\include\\Path\\AStar.h .\\include\\D2Common\\Path\\AStarVisitorDefinitions.h
+python ..\\generate_visitor_definitions.py ..\\D2Common\\include\\Path\\IDAStar.h .\\include\\D2Common\\Path\\IDAStarVisitorDefinitions.h
+python ..\\generate_visitor_definitions.py ..\\D2Common\\include\\Path\\Path.h .\\include\\D2Common\\Path\\PathVisitorDefinitions.h
+
+# Units
+
+python ..\\generate_visitor_definitions.py ..\\D2Common\\include\\Units\\Item.h .\\include\\D2Common\\Units\\ItemVisitorDefinitions.h
+python ..\\generate_visitor_definitions.py ..\\D2Common\\include\\Units\\Missile.h .\\include\\D2Common\\Units\\MissileVisitorDefinitions.h
+python ..\\generate_visitor_definitions.py ..\\D2Common\\include\\Units\\MissileStream.h .\\include\\D2Common\\Units\\MissileStreamVisitorDefinitions.h
+python ..\\generate_visitor_definitions.py ..\\D2Common\\include\\Units\\Monster.h .\\include\\D2Common\\Units\\MonsterVisitorDefinitions.h
+python ..\\generate_visitor_definitions.py ..\\D2Common\\include\\Units\\Object.h .\\include\\D2Common\\Units\\ObjectVisitorDefinitions.h
+python ..\\generate_visitor_definitions.py ..\\D2Common\\include\\Units\\Player.h .\\include\\D2Common\\Units\\PlayerVisitorDefinitions.h
+python ..\\generate_visitor_definitions.py ..\\D2Common\\include\\Units\\UnitFinds.h .\\include\\D2Common\\Units\\UnitFindsVisitorDefinitions.h
+python ..\\generate_visitor_definitions.py ..\\D2Common\\include\\Units\\Units.h .\\include\\D2Common\\Units\\UnitsVisitorDefinitions.h
+
+# General
+
+python ..\\generate_visitor_definitions.py ..\\D2Common\\include\\D2Chat.h .\\include\\D2Common\\D2ChatVisitorDefinitions.h
+python ..\\generate_visitor_definitions.py ..\\D2Common\\include\\D2Collision.h .\\include\\D2Common\\D2CollisionVisitorDefinitions.h
+python ..\\generate_visitor_definitions.py ..\\D2Common\\include\\D2Composit.h .\\include\\D2Common\\D2CompositVisitorDefinitions.h
+python ..\\generate_visitor_definitions.py ..\\D2Common\\include\\D2DataTbls.h .\\include\\D2Common\\D2DataTblsVisitorDefinitions.h
+python ..\\generate_visitor_definitions.py ..\\D2Common\\include\\D2Environment.h .\\include\\D2Common\\D2EnvironmentVisitorDefinitions.h
+python ..\\generate_visitor_definitions.py ..\\D2Common\\include\\D2Inventory.h .\\include\\D2Common\\D2InventoryVisitorDefinitions.h
+python ..\\generate_visitor_definitions.py ..\\D2Common\\include\\D2ItemMods.h .\\include\\D2Common\\D2ItemModsVisitorDefinitions.h
+python ..\\generate_visitor_definitions.py ..\\D2Common\\include\\D2Items.h .\\include\\D2Common\\D2ItemsVisitorDefinitions.h
+python ..\\generate_visitor_definitions.py ..\\D2Common\\include\\D2Monsters.h .\\include\\D2Common\\D2MonstersVisitorDefinitions.h
+python ..\\generate_visitor_definitions.py ..\\D2Common\\include\\D2Seed.h .\\include\\D2Common\\D2SeedVisitorDefinitions.h
+python ..\\generate_visitor_definitions.py ..\\D2Common\\include\\D2Skills.h .\\include\\D2Common\\D2SkillsVisitorDefinitions.h
+python ..\\generate_visitor_definitions.py ..\\D2Common\\include\\D2StatList.h .\\include\\D2Common\\D2StatListVisitorDefinitions.h
+python ..\\generate_visitor_definitions.py ..\\D2Common\\include\\D2Text.h .\\include\\D2Common\\D2TextVisitorDefinitions.h
+python ..\\generate_visitor_definitions.py ..\\D2Common\\include\\D2Waypoints.h .\\include\\D2Common\\D2WaypointsVisitorDefinitions.h
+
+
+# D2Game
+
+# AI
+
+python ..\\generate_visitor_definitions.py ..\\D2Game\\include\\AI\\AiGeneral.h .\\include\\D2Game\\AI\\AiGeneralVisitorDefinitions.h
+python ..\\generate_visitor_definitions.py ..\\D2Game\\include\\AI\\AiTactics.h .\\include\\D2Game\\AI\\AiTacticsVisitorDefinitions.h
+python ..\\generate_visitor_definitions.py ..\\D2Game\\include\\AI\\AiUtil.h .\\include\\D2Game\\AI\\AiUtilVisitorDefinitions.h
+
+
+# GAME
+
+python ..\\generate_visitor_definitions.py ..\\D2Game\\include\\GAME\\Arena.h .\\include\\D2Game\\GAME\\ArenaVisitorDefinitions.h
+python ..\\generate_visitor_definitions.py ..\\D2Game\\include\\GAME\\Clients.h .\\include\\D2Game\\GAME\\ClientsVisitorDefinitions.h
+python ..\\generate_visitor_definitions.py ..\\D2Game\\include\\GAME\\Event.h .\\include\\D2Game\\GAME\\EventVisitorDefinitions.h
+python ..\\generate_visitor_definitions.py ..\\D2Game\\include\\GAME\\Game.h .\\include\\D2Game\\GAME\\GameVisitorDefinitions.h
+python ..\\generate_visitor_definitions.py ..\\D2Game\\include\\GAME\\Task.h .\\include\\D2Game\\GAME\\TaskVisitorDefinitions.h
+
+
+# ITEMS
+
+python ..\\generate_visitor_definitions.py ..\\D2Game\\include\\ITEMS\\ItemMode.h .\\include\\D2Game\\ITEMS\\ItemModeVisitorDefinitions.h
+python ..\\generate_visitor_definitions.py ..\\D2Game\\include\\ITEMS\\Items.h .\\include\\D2Game\\ITEMS\\ItemsVisitorDefinitions.h
+
+
+# MISSILES
+
+python ..\\generate_visitor_definitions.py ..\\D2Game\\include\\MISSILES\\MissMode.h .\\include\\D2Game\\MISSILES\\MissModeVisitorDefinitions.h
+
+
+# MONSTER
+
+python ..\\generate_visitor_definitions.py ..\\D2Game\\include\\MONSTER\\MonsterAI.h .\\include\\D2Game\\MONSTER\\MonsterAIVisitorDefinitions.h
+python ..\\generate_visitor_definitions.py ..\\D2Game\\include\\MONSTER\\MonsterMode.h .\\include\\D2Game\\MONSTER\\MonsterModeVisitorDefinitions.h
+python ..\\generate_visitor_definitions.py ..\\D2Game\\include\\MONSTER\\MonsterRegion.h .\\include\\D2Game\\MONSTER\\MonsterRegionVisitorDefinitions.h
+python ..\\generate_visitor_definitions.py ..\\D2Game\\include\\MONSTER\\MonsterSpawn.h .\\include\\D2Game\\MONSTER\\MonsterSpawnVisitorDefinitions.h
+
+
+# OBJECTS
+
+python ..\\generate_visitor_definitions.py ..\\D2Game\\include\\OBJECTS\\Objects.h .\\include\\D2Game\\OBJECTS\\ObjectsVisitorDefinitions.h
+python ..\\generate_visitor_definitions.py ..\\D2Game\\include\\OBJECTS\\ObjMode.h .\\include\\D2Game\\OBJECTS\\ObjModeVisitorDefinitions.h
+
+
+# PLAYER
+
+python ..\\generate_visitor_definitions.py ..\\D2Game\\include\\PLAYER\\PlrModes.h .\\include\\D2Game\\PLAYER\\PlrModesVisitorDefinitions.h
+python ..\\generate_visitor_definitions.py ..\\D2Game\\include\\PLAYER\\PlrSave.h .\\include\\D2Game\\PLAYER\\PlrSaveVisitorDefinitions.h
+python ..\\generate_visitor_definitions.py ..\\D2Game\\include\\PLAYER\\PlrSave2.h .\\include\\D2Game\\PLAYER\\PlrSave2VisitorDefinitions.h
+python ..\\generate_visitor_definitions.py ..\\D2Game\\include\\PLAYER\\PlrTrade.h .\\include\\D2Game\\PLAYER\\PlrTradeVisitorDefinitions.h
+
+
+# QUESTS
+
+python ..\\generate_visitor_definitions.py ..\\D2Game\\include\\QUESTS\\ACT1\\A1Q1.h .\\include\\D2Game\\QUESTS\\ACT1\\A1Q1VisitorDefinitions.h
+python ..\\generate_visitor_definitions.py ..\\D2Game\\include\\QUESTS\\ACT1\\A1Q2.h .\\include\\D2Game\\QUESTS\\ACT1\\A1Q2VisitorDefinitions.h
+python ..\\generate_visitor_definitions.py ..\\D2Game\\include\\QUESTS\\ACT1\\A1Q3.h .\\include\\D2Game\\QUESTS\\ACT1\\A1Q3VisitorDefinitions.h
+python ..\\generate_visitor_definitions.py ..\\D2Game\\include\\QUESTS\\ACT1\\A1Q4.h .\\include\\D2Game\\QUESTS\\ACT1\\A1Q4VisitorDefinitions.h
+python ..\\generate_visitor_definitions.py ..\\D2Game\\include\\QUESTS\\ACT1\\A1Q5.h .\\include\\D2Game\\QUESTS\\ACT1\\A1Q5VisitorDefinitions.h
+python ..\\generate_visitor_definitions.py ..\\D2Game\\include\\QUESTS\\ACT1\\A1Q6.h .\\include\\D2Game\\QUESTS\\ACT1\\A1Q6VisitorDefinitions.h
+
+python ..\\generate_visitor_definitions.py ..\\D2Game\\include\\QUESTS\\ACT2\\A2Q0.h .\\include\\D2Game\\QUESTS\\ACT2\\A2Q0VisitorDefinitions.h
+python ..\\generate_visitor_definitions.py ..\\D2Game\\include\\QUESTS\\ACT2\\A2Q1.h .\\include\\D2Game\\QUESTS\\ACT2\\A2Q1VisitorDefinitions.h
+python ..\\generate_visitor_definitions.py ..\\D2Game\\include\\QUESTS\\ACT2\\A2Q2.h .\\include\\D2Game\\QUESTS\\ACT2\\A2Q2VisitorDefinitions.h
+python ..\\generate_visitor_definitions.py ..\\D2Game\\include\\QUESTS\\ACT2\\A2Q3.h .\\include\\D2Game\\QUESTS\\ACT2\\A2Q3VisitorDefinitions.h
+python ..\\generate_visitor_definitions.py ..\\D2Game\\include\\QUESTS\\ACT2\\A2Q4.h .\\include\\D2Game\\QUESTS\\ACT2\\A2Q4VisitorDefinitions.h
+python ..\\generate_visitor_definitions.py ..\\D2Game\\include\\QUESTS\\ACT2\\A2Q5.h .\\include\\D2Game\\QUESTS\\ACT2\\A2Q5VisitorDefinitions.h
+python ..\\generate_visitor_definitions.py ..\\D2Game\\include\\QUESTS\\ACT2\\A2Q6.h .\\include\\D2Game\\QUESTS\\ACT2\\A2Q6VisitorDefinitions.h
+python ..\\generate_visitor_definitions.py ..\\D2Game\\include\\QUESTS\\ACT2\\A2Q7.h .\\include\\D2Game\\QUESTS\\ACT2\\A2Q7VisitorDefinitions.h
+python ..\\generate_visitor_definitions.py ..\\D2Game\\include\\QUESTS\\ACT2\\A2Q8.h .\\include\\D2Game\\QUESTS\\ACT2\\A2Q8VisitorDefinitions.h
+
+python ..\\generate_visitor_definitions.py ..\\D2Game\\include\\QUESTS\\ACT3\\A3Q0.h .\\include\\D2Game\\QUESTS\\ACT3\\A3Q0VisitorDefinitions.h
+python ..\\generate_visitor_definitions.py ..\\D2Game\\include\\QUESTS\\ACT3\\A3Q1.h .\\include\\D2Game\\QUESTS\\ACT3\\A3Q1VisitorDefinitions.h
+python ..\\generate_visitor_definitions.py ..\\D2Game\\include\\QUESTS\\ACT3\\A3Q2.h .\\include\\D2Game\\QUESTS\\ACT3\\A3Q2VisitorDefinitions.h
+python ..\\generate_visitor_definitions.py ..\\D2Game\\include\\QUESTS\\ACT3\\A3Q3.h .\\include\\D2Game\\QUESTS\\ACT3\\A3Q3VisitorDefinitions.h
+python ..\\generate_visitor_definitions.py ..\\D2Game\\include\\QUESTS\\ACT3\\A3Q4.h .\\include\\D2Game\\QUESTS\\ACT3\\A3Q4VisitorDefinitions.h
+python ..\\generate_visitor_definitions.py ..\\D2Game\\include\\QUESTS\\ACT3\\A3Q5.h .\\include\\D2Game\\QUESTS\\ACT3\\A3Q5VisitorDefinitions.h
+python ..\\generate_visitor_definitions.py ..\\D2Game\\include\\QUESTS\\ACT3\\A3Q6.h .\\include\\D2Game\\QUESTS\\ACT3\\A3Q6VisitorDefinitions.h
+python ..\\generate_visitor_definitions.py ..\\D2Game\\include\\QUESTS\\ACT3\\A3Q7.h .\\include\\D2Game\\QUESTS\\ACT3\\A3Q7VisitorDefinitions.h
+
+python ..\\generate_visitor_definitions.py ..\\D2Game\\include\\QUESTS\\ACT4\\A4Q1.h .\\include\\D2Game\\QUESTS\\ACT4\\A4Q1VisitorDefinitions.h
+python ..\\generate_visitor_definitions.py ..\\D2Game\\include\\QUESTS\\ACT4\\A4Q2.h .\\include\\D2Game\\QUESTS\\ACT4\\A4Q2VisitorDefinitions.h
+python ..\\generate_visitor_definitions.py ..\\D2Game\\include\\QUESTS\\ACT4\\A4Q3.h .\\include\\D2Game\\QUESTS\\ACT4\\A4Q3VisitorDefinitions.h
+
+python ..\\generate_visitor_definitions.py ..\\D2Game\\include\\QUESTS\\ACT5\\A5Q1.h .\\include\\D2Game\\QUESTS\\ACT5\\A5Q1VisitorDefinitions.h
+python ..\\generate_visitor_definitions.py ..\\D2Game\\include\\QUESTS\\ACT5\\A5Q2.h .\\include\\D2Game\\QUESTS\\ACT5\\A5Q2VisitorDefinitions.h
+python ..\\generate_visitor_definitions.py ..\\D2Game\\include\\QUESTS\\ACT5\\A5Q3.h .\\include\\D2Game\\QUESTS\\ACT5\\A5Q3VisitorDefinitions.h
+python ..\\generate_visitor_definitions.py ..\\D2Game\\include\\QUESTS\\ACT5\\A5Q4.h .\\include\\D2Game\\QUESTS\\ACT5\\A5Q4VisitorDefinitions.h
+python ..\\generate_visitor_definitions.py ..\\D2Game\\include\\QUESTS\\ACT5\\A5Q5.h .\\include\\D2Game\\QUESTS\\ACT5\\A5Q5VisitorDefinitions.h
+python ..\\generate_visitor_definitions.py ..\\D2Game\\include\\QUESTS\\ACT5\\A5Q6.h .\\include\\D2Game\\QUESTS\\ACT5\\A5Q6VisitorDefinitions.h
+
+python ..\\generate_visitor_definitions.py ..\\D2Game\\include\\QUESTS\\Quests.h .\\include\\D2Game\\QUESTS\\QuestsVisitorDefinitions.h
+
+
+# SKILLS
+
+python ..\\generate_visitor_definitions.py ..\\D2Game\\include\\SKILLS\\SkillItem.h .\\include\\D2Game\\SKILLS\\SkillItemVisitorDefinitions.h
+python ..\\generate_visitor_definitions.py ..\\D2Game\\include\\SKILLS\\Skills.h .\\include\\D2Game\\SKILLS\\SkillsVisitorDefinitions.h
+
+
+# UNIT
+
+python ..\\generate_visitor_definitions.py ..\\D2Game\\include\\UNIT\\Party.h .\\include\\D2Game\\UNIT\\PartyVisitorDefinitions.h
+python ..\\generate_visitor_definitions.py ..\\D2Game\\include\\UNIT\\SUnit.h .\\include\\D2Game\\UNIT\\SUnitVisitorDefinitions.h
+python ..\\generate_visitor_definitions.py ..\\D2Game\\include\\UNIT\\SUnitDmg.h .\\include\\D2Game\\UNIT\\SUnitDmgVisitorDefinitions.h
+python ..\\generate_visitor_definitions.py ..\\D2Game\\include\\UNIT\\SUnitEvent.h .\\include\\D2Game\\UNIT\\SUnitEventVisitorDefinitions.h
+python ..\\generate_visitor_definitions.py ..\\D2Game\\include\\UNIT\\SUnitInactive.h .\\include\\D2Game\\UNIT\\SUnitInactiveVisitorDefinitions.h
+python ..\\generate_visitor_definitions.py ..\\D2Game\\include\\UNIT\\SUnitNpc.h .\\include\\D2Game\\UNIT\\SUnitNpcVisitorDefinitions.h
+python ..\\generate_visitor_definitions.py ..\\D2Game\\include\\UNIT\\SUnitProxy.h .\\include\\D2Game\\UNIT\\SUnitProxyVisitorDefinitions.h
+
+
+# General
+
+python ..\\generate_visitor_definitions.py ..\\D2Game\\include\\D2Packet.h .\\include\\D2Game\\D2PacketVisitorDefinitions.h
