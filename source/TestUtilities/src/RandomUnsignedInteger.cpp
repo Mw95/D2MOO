b7a9d7e6ac7457857f497c0d5803b6d20f594a2b
Mnw1995 <mnw@1995.com>
2026-09-10 22:54:36 +0200

    fix(test_utilities_setup): Fix random_unsigned_integer

--- source/TestUtilities/src/RandomUnsignedInteger.cpp
+++ source/TestUtilities/src/RandomUnsignedInteger.cpp
@@ -5,7 +5,7 @@
 #include <random>
 
 
-static auto random_unsigned_integer(uint32_t min, uint32_t max) -> uint32_t
+auto random_unsigned_integer(uint32_t min, uint32_t max) -> uint32_t
 {
 	static std::mt19937 generator(std::random_device{}());
 	std::uniform_int_distribution<uint32_t> distribution(min, max);
