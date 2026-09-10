c97317e7947413dc497c61b37a394084f9294f58
Mnw1995 <mnw@1995.com>
2026-09-10 22:54:36 +0200

    fix(test_utilities_setup): Fix random_unsigned_integer

--- source/TestUtilities/include/RandomUnsignedInteger.h
+++ source/TestUtilities/include/RandomUnsignedInteger.h
@@ -5,4 +5,4 @@
 #include <limits>
 
 
-auto random__unsigned_integer(uint32_t min = 0, uint32_t max = std::numeric_limits<uint32_t>::max()) -> uint32_t;
+auto random_unsigned_integer(uint32_t min = 0, uint32_t max = std::numeric_limits<uint32_t>::max()) -> uint32_t;
