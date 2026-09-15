5cb554f767efb32e516445736e22b4e804876245
Mnw1995 <mnw@1995.com>
2026-09-16 00:21:09 +0200

    add(add_scripts): Add script to automatically generate fixtures for .txt data tables

---
+++ source/TestDefinitions/generate_txt_fixtures.sh
@@ -0,0 +1,67 @@
+# python ..\\generate_txt_fixtures.py include/Fixtures/DataTbls/ArmorTxtFixture.h Armor D2ArmorTxt 0x00000000 0x00000000
+# python ..\\generate_txt_fixtures.py include/Fixtures/DataTbls/AutoMagicTxtFixture.h AutoMagic D2ArmorTxt 0x00000000 0x00000000
+# python ..\\generate_txt_fixtures.py include/Fixtures/DataTbls/BooksTxtFixture.h Books D2ArmorTxt 0x00000000 0x00000000
+python ..\\generate_txt_fixtures.py include/Fixtures/DataTbls/CharStatsTxtFixture.h CharStats D2CharStatsTxt 0x00000BC4 0x00000BC8
+python ..\\generate_txt_fixtures.py include/Fixtures/DataTbls/CompCodeTxtFixture.h CompCode D2CompCodeTxt 0x0000007C 0x00000084
+python ..\\generate_txt_fixtures.py include/Fixtures/DataTbls/CubeMainTxtFixture.h CubeMain D2CubeMainTxt 0x00000CC4 0x00000CC8
+python ..\\generate_txt_fixtures.py include/Fixtures/DataTbls/DifficultyLevelsTxtFixture.h DifficultyLevels D2DifficultyLevelsTxt 0x00000C7C 0x00000C80
+python ..\\generate_txt_fixtures.py include/Fixtures/DataTbls/ElemTypesTxtFixture.h ElemTypes D2ElemTypesTxt 0x00000018
+python ..\\generate_txt_fixtures.py include/Fixtures/DataTbls/EventsTxtFixture.h Events D2EventsTxt 0x00000074
+# python ..\\generate_txt_fixtures.py include/Fixtures/DataTbls/ExperienceTxtFixture.h Experience D2ArmorTxt 0x00000000 0x00000000
+# python ..\\generate_txt_fixtures.py include/Fixtures/DataTbls/GemsTxtFixture.h Gems D2ArmorTxt 0x00000000 0x00000000
+python ..\\generate_txt_fixtures.py include/Fixtures/DataTbls/HirelingTxtFixture.h Hireling D2HirelingTxt 0x000001A0 0x000001A4
+# python ..\\generate_txt_fixtures.py include/Fixtures/DataTbls/ItemRatioTxtFixture.h ItemRatio D2ArmorTxt 0x00000000 0x00000000
+# python ..\\generate_txt_fixtures.py include/Fixtures/DataTbls/ItemsCodeTxtFixture.h ItemsCode D2ArmorTxt 0x00000000 0x00000000
+python ..\\generate_txt_fixtures.py include/Fixtures/DataTbls/ItemStatCostTxtFixture.h ItemStatCost D2ItemStatCostTxt 0x00000BCC 0x00000BD4
+python ..\\generate_txt_fixtures.py include/Fixtures/DataTbls/ItemTypesTxtFixture.h ItemTypes D2ItemTypesTxt 0x00000BF8 0x00000BFC
+python ..\\generate_txt_fixtures.py include/Fixtures/DataTbls/LevelDefsTxtFixture.h LevelDefs D2LevelDefBin 0x00000C60
+python ..\\generate_txt_fixtures.py include/Fixtures/DataTbls/LevelsTxtFixture.h Levels D2LevelsTxt 0x00000C58 0x00000C5C
+python ..\\generate_txt_fixtures.py include/Fixtures/DataTbls/LvlMazeTxtFixture.h LvlMaze D2LvlMazeTxt 0x00001030 0x00001034
+python ..\\generate_txt_fixtures.py include/Fixtures/DataTbls/LvlPrestTxtFixture.h LvlPrest D2LvlPrestTxt 0x00000C64 0x00000C68
+python ..\\generate_txt_fixtures.py include/Fixtures/DataTbls/LvlSubTxtFixture.h LvlSub D2LvlSubTxt 0x00001038 0x0000103C
+python ..\\generate_txt_fixtures.py include/Fixtures/DataTbls/LvlTypesTxtFixture.h LvlTypes D2LvlTypesTxt 0x00001018 0x00001024
+# python ..\\generate_txt_fixtures.py include/Fixtures/DataTbls/MagicPrefixTxtFixture.h MagicPrefix D2ArmorTxt 0x00000000 0x00000000
+# python ..\\generate_txt_fixtures.py include/Fixtures/DataTbls/MagicSuffixTxtFixture.h MagicSuffix D2ArmorTxt 0x00000000 0x00000000
+# python ..\\generate_txt_fixtures.py include/Fixtures/DataTbls/MiscTxtFixture.h Misc D2ArmorTxt 0x00000000 0x00000000
+python ..\\generate_txt_fixtures.py include/Fixtures/DataTbls/MissCalcTxtFixture.h MissCalc D2MissCalcTxt 0x00000058
+# python ..\\generate_txt_fixtures.py include/Fixtures/DataTbls/MissCodeTxtFixture.h MissCode D2ArmorTxt 0x00000000 0x00000000
+python ..\\generate_txt_fixtures.py include/Fixtures/DataTbls/MissilesTxtFixture.h Missiles D2MissilesTxt 0x00000B64 0x00000B6C
+python ..\\generate_txt_fixtures.py include/Fixtures/DataTbls/MonAiTxtFixture.h MonAi D2MonAiTxt 0x00000088 0x00000090
+python ..\\generate_txt_fixtures.py include/Fixtures/DataTbls/MonEquipTxtFixture.h MonEquip D2MonEquipTxt 0x00000BE0 0x00000BE4
+python ..\\generate_txt_fixtures.py include/Fixtures/DataTbls/MonLvlTxtFixture.h MonLvl D2MonLvlTxt 0x00000B70 0x00000B74
+# python ..\\generate_txt_fixtures.py include/Fixtures/DataTbls/MonModeTxtFixture.h MonMode D2ArmorTxt 0x00000000 0x00000000
+python ..\\generate_txt_fixtures.py include/Fixtures/DataTbls/MonPlaceTxtFixture.h MonPlace D2MonPlaceTxt 0x00000A9C 0x00000AA4
+python ..\\generate_txt_fixtures.py include/Fixtures/DataTbls/MonPresetTxtFixture.h MonPreset D2MonPresetTxt 0x00000AA8
+python ..\\generate_txt_fixtures.py include/Fixtures/DataTbls/MonPropTxtFixture.h MonProp D2MonPropTxt 0x00000C30 0x00000C34
+python ..\\generate_txt_fixtures.py include/Fixtures/DataTbls/MonSeqTxtFixture.h MonSeq D2AnimSeqTxt 0x0000B78 0x00000B80
+python ..\\generate_txt_fixtures.py include/Fixtures/DataTbls/MonSoundsTxtFixture.h MonSounds D2MonSoundsTxt 0x00000A84 0x00000A8C
+python ..\\generate_txt_fixtures.py include/Fixtures/DataTbls/MonStatsTxtFixture.h MonStats D2MonStatsTxt 0x00000A78 0x00000A80
+python ..\\generate_txt_fixtures.py include/Fixtures/DataTbls/MonStats2TxtFixture.h MonStats2 D2MonStats2Txt 0x00000A90 0x00000A98
+python ..\\generate_txt_fixtures.py include/Fixtures/DataTbls/MonTypeTxtFixture.h MonType D2MonTypeTxt 0x00000C3C 0x00000C40
+python ..\\generate_txt_fixtures.py include/Fixtures/DataTbls/MonUModTxtFixture.h MonUMod D2MonUModTxt 0x00000C50 0x00000C54
+python ..\\generate_txt_fixtures.py include/Fixtures/DataTbls/NpcTxtFixture.h Npc D2NpcTxt 0x000009A8 0x000009AC
+python ..\\generate_txt_fixtures.py include/Fixtures/DataTbls/ObjectsTxtFixture.h Objects D2ObjectsTxt 0x0000109C 0x000010A0
+python ..\\generate_txt_fixtures.py include/Fixtures/DataTbls/OverlayTxtFixture.h Overlay D2OverlayTxt 0x00000BBC 0x00000BC0
+python ..\\generate_txt_fixtures.py include/Fixtures/DataTbls/PetTypeTxtFixture.h PetType D2PetTypeTxt 0x00000BE8 0x00000BF0
+python ..\\generate_txt_fixtures.py include/Fixtures/DataTbls/PropertiesTxtFixture.h Properties D2PropertiesTxt 0x000000A4 0x000000AC
+# python ..\\generate_txt_fixtures.py include/Fixtures/DataTbls/RarePrefixTxtFixture.h RarePrefix D2ArmorTxt 0x00000000 0x00000000
+# python ..\\generate_txt_fixtures.py include/Fixtures/DataTbls/RareSuffixTxtFixture.h RareSuffix D2ArmorTxt 0x00000000 0x00000000
+# python ..\\generate_txt_fixtures.py include/Fixtures/DataTbls/RunesTxtFixture.h Runes D2ArmorTxt 0x00000000 0x00000000
+python ..\\generate_txt_fixtures.py include/Fixtures/DataTbls/SetItemsTxtFixture.h SetItems D2SetItemsTxt 0x00000C18 0x00000C1C
+python ..\\generate_txt_fixtures.py include/Fixtures/DataTbls/SetsTxtFixture.h Sets D2SetsTxt 0x00000C0C 0x00000C10
+python ..\\generate_txt_fixtures.py include/Fixtures/DataTbls/ShrinesTxtFixture.h Shrines D2ShrinesTxt 0x00001094 0x00001098
+python ..\\generate_txt_fixtures.py include/Fixtures/DataTbls/SkillCalcTxtFixture.h SkillCalc D2SkillCalcTxt 0x00000038
+python ..\\generate_txt_fixtures.py include/Fixtures/DataTbls/SkillDescTxtFixture.h SkillDesc D2SkillDescTxt 0x00000B8C 0x00000B94
+# python ..\\generate_txt_fixtures.py include/Fixtures/DataTbls/SkillDescCodeTxtFixture.h SkillDescCode D2ArmorTxt 0x00000000 0x00000000
+python ..\\generate_txt_fixtures.py include/Fixtures/DataTbls/SkillsTxtFixture.h Skills D2SkillsTxt 0x00000B98 0x00000BA0
+# python ..\\generate_txt_fixtures.py include/Fixtures/DataTbls/SkillsCodeTxtFixture.h SkillsCode D2ArmorTxt 0x00000000 0x00000000
+# python ..\\generate_txt_fixtures.py include/Fixtures/DataTbls/SoundsTxtFixture.h Sounds D2ArmorTxt 0x00000000 0x00000000
+python ..\\generate_txt_fixtures.py include/Fixtures/DataTbls/StatesTxtFixture.h States D2StatesTxt 0x000000BC 0x000000C4
+python ..\\generate_txt_fixtures.py include/Fixtures/DataTbls/SuperUniquesTxtFixture.h SuperUniques D2SuperUniquesTxt 0x00000AD4 0x00000ADC
+# python ..\\generate_txt_fixtures.py include/Fixtures/DataTbls/TreasureClassExTxtFixture.h TreasureClassEx D2ArmorTxt 0x00000000 0x00000000
+python ..\\generate_txt_fixtures.py include/Fixtures/DataTbls/UniqueAppellationTxtFixture.h UniqueAppellation D2UniqueAppellationTxt 0x00001074 0x00001084
+python ..\\generate_txt_fixtures.py include/Fixtures/DataTbls/UniqueItemsTxtFixture.h UniqueItems D2UniqueItemsTxt 0x00000C24 0x00000C28
+python ..\\generate_txt_fixtures.py include/Fixtures/DataTbls/UniquePrefixTxtFixture.h UniquePrefix D2UniquePrefixTxt 0x0000106C 0x0000107C
+python ..\\generate_txt_fixtures.py include/Fixtures/DataTbls/UniqueSuffixTxtFixture.h UniqueSuffix D2UniqueSuffixTxt 0x00001070 0x00001080
+python ..\\generate_txt_fixtures.py include/Fixtures/DataTbls/UniqueTitleTxtFixture.h UniqueTitle D2UniqueTitleTxt 0x00001068 0x00001078
+# python ..\\generate_txt_fixtures.py include/Fixtures/DataTbls/WeaponsTxtFixture.h Weapons D2ArmorTxt 0x00000000 0x00000000
